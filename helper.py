a98567fe5a63db5d6cd33a5b153876253b0059e0
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-09-21 23:28:02 -0400

    Version 2 release

--- helper.py
+++ helper.py
@@ -1,40 +1,11 @@
-from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 import requests
 
 
-def id_grabber(youtube_url):
-    youtube_id = None
-    # reference: https://stackoverflow.com/questions/4356538/how-can-i-extract-video-id-from-youtubes-link-in-python/54383711#54383711
-    query = urlparse(youtube_url)
-
-    if query.hostname == "youtu.be":
-        youtube_id = query.path[1:]
-
-    elif query.hostname in {"www.youtube.com", "youtube.com"}:
-        if query.path == "/watch":
-            youtube_id = parse_qs(query.query)["v"][0]
-
-        elif query.path[:7] == "/watch/":
-            youtube_id = query.path.split("/")[1]
-
-        elif query.path[:7] == "/embed/":
-            youtube_id = query.path.split("/")[2]
-
-        elif query.path[:3] == "/v/":
-            youtube_id = query.path.split("/")[2]
-
-        # below is optional for playlists
-        elif query.path[:9] == "/playlist":
-            youtube_id = parse_qs(query.query)["list"][0]
-
-    return youtube_id
-
-
-def webscrapper(youtube_url):
+def webscrapper(user_url):
     video_details = {}
-    # print(youtube_url)
-    req = requests.get(youtube_url)
+
+    req = requests.get(user_url)
     soup = BeautifulSoup(req.content, 'html.parser')
 
     video_details['video_title'] = soup.find(
@@ -43,8 +14,8 @@ def webscrapper(youtube_url):
     video_details['video_author'] = soup.find(
         "link", itemprop="name").attrs.get('content')
 
-    video_details['video_views'] = soup.find(
-        "meta", itemprop="interactionCount").attrs.get('content')
+    video_details['video_views'] = "{:,}".format(int(soup.find(
+        "meta", itemprop="interactionCount").attrs.get('content')))
 
     video_details['video_published'] = soup.find(
         "meta", itemprop="datePublished").attrs.get('content')
@@ -57,6 +28,9 @@ def webscrapper(youtube_url):
 
     video_details['video_thumbnail'] = soup.find(
         "link", itemprop="thumbnailUrl").attrs.get('href')
+
+    video_details[
+        'video_url'] = f"https://www.youtube.com/watch?v={video_details.get('video_id')}"
 
     print(video_details)
 
